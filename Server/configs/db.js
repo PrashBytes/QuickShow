b300afc3b5044e892c25b61d87cb63ceafe54e48
PrashBytes <prashantasarmahbordoloi@email.com>
2026-04-24 23:00:32 +0530

    Fix MongoDB connection and Inngest setup

--- Server/configs/db.js
+++ Server/configs/db.js
@@ -2,10 +2,12 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/quickshow");
+    await mongoose.connect(
+      process.env.MONGODB_URL);
+
     console.log("Database connected");
   } catch (error) {
-    console.log(error.message);
+    console.log(error);
   }
 };
 
